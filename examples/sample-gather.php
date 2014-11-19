ddf98bad1b39ae12e826f4e748552148c70b6f4b
Nadir Hamid <matrix.nad@gmail.com>
2014-11-19 11:09:32 -0700

    Added: call gather example

---
+++ examples/sample-gather.php
@@ -0,0 +1,52 @@
+<?php
+require_once('../source/Catapult.php');
+
+// below is a sample gather test
+// it will call the number, prompt for
+// a gather and if it is valid
+// show a message. if not repeat
+// IMPORTANT: edit credentials.json
+// with your information
+// or comment out below /w your keys
+//
+$cred = new Catapult\Credentials('USER_ID', 'API_TOKEN', 'API_SECRET');
+//$cred = new Catapult\Credentials;
+// dont forget to comment out the implicit version if using assoc array
+
+$client = new Catapult\Client($cred);
+
+
+try {
+	$call = new Catapult\Call;
+
+	$call->create(array(
+		"from" => $argv[1],
+		"to" => $argv[2]
+	));
+
+	$call->wait();
+
+	$gather = new Catapult\Gather($call->id);
+
+	$gather->create(array(
+		"maxDigits" => 10
+	));
+
+	$call->speakSentence(array(
+		"sentence" => "Please enter your digits."	
+	));
+
+	if ($gather->check("digits", $argv[3])) 
+		$call->speakSentence(array(
+			"sentence" => "Correct."		
+		));
+	else
+		$call->speakSentence(array(
+			"sentence" => "The digits were wrong"
+		));
+	
+
+} catch (\CatapultApiException $e) {
+	echo var_dump($e);	
+}
+?>
