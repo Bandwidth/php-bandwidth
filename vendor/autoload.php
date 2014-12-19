9945ac1693cff342e6737b8f8013891067de6df8
Nadir Hamid <matrix.nad@gmail.com>
2014-12-19 18:19:18 +0000

    mininum stability version, in dev mode

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInitd75dd656e75d5c5cd3e3083d051c9c91::getLoader();
+return ComposerAutoloaderInit7e04dcba36c95a64b4027626f558ee87::getLoader();
