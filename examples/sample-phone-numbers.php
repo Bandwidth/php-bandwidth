021b773fd05dc719af261ab59172b4f65db59892
nadirhamid <matrix.nad@gmail.com>
2014-12-20 23:58:07 -0600

    Phone Number listing example

---
+++ examples/sample-phone-numbers.php
@@ -0,0 +1,46 @@
+<?php
+require_once('../source/Catapult.php');
+
+// below is a sample text message
+// using Catapult's Phone Number alloc 
+//
+// IMPORTANT: edit credentials.json
+// with your information
+// or comment out below /w your keys
+//
+//$cred = new Catapult\Credentials('BANDWIDTH_USER_ID', 'BANDWIDTH_API_TOKEN', 'BANDWIDTH_API_SECRET');
+
+$cred = new Catapult\Credentials;
+// dont forget to comment out the implicit version if using assoc array
+
+// this example is cli based
+// use like:
+// php ./sample-message.php "+from" "+to" "message"
+
+$client = new Catapult\Client($cred);
+
+try {
+	$number_service = new Catapult\PhoneNumbers;
+	
+	$numbers = $number_service->listNumbers();
+
+	/** get the info for each number listed under this account **/
+
+	printf("You have listed the following numbers under your account \n");
+
+	foreach ($numbers->get() as $number) {
+		printf("You have registered number: %d, on %s\n", $number->number, $number->createdTime);
+
+		/** get even more information on this number using numberService **/
+
+		$info = new Catapult\NumberInfo;
+		$info->get($number->number);
+
+
+		printf("Number area: %s\n", $info->name);	
+	}
+
+} catch (\CatapultApiException $e) {
+	echo var_dump($e);	
+}
+?>
