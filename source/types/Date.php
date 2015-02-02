7f0aa4c3faf4083bf584fde010f00b909234ea10
Nadir Hamid <matrix.nad@gmail.com>
2015-02-02 04:22:54 +0000

    fix padding

--- source/types/Date.php
+++ source/types/Date.php
@@ -11,25 +11,25 @@
  */
 namespace Catapult;
 final class Date extends Types {
-	/** 
-     * Input should allow datetime objects
-	 * or unix stamps.
-	 */
-	public function __construct($datetime)
-	{
-    if (is_int($datetime)) {
-      $dt = new \DateTime();	
-      $dt->setTimestamp($datetime);
-    } else {
-      $dt = $datetime;
-    }
+   /** 
+    * Input should allow datetime objects
+    * or unix stamps.
+    */
+    public function __construct($datetime)
+    {
+      if (is_int($datetime)) {
+        $dt = new \DateTime();	
+        $dt->setTimestamp($datetime);
+      } else {
+        $dt = $datetime;
+      }
 
-    $this->date = $dt;
-	}
-	public function __toString()
-	{
-		return $this->date->format(API::API_DATE_FORMAT);	
-	}
+      $this->date = $dt;
+    }
+    public function __toString()
+    {
+      return $this->date->format(API::API_DATE_FORMAT);	
+    }
 }
 
 ?>
--- source/types/PhoneNumber.php
+++ source/types/PhoneNumber.php
@@ -5,26 +5,21 @@ namespace Catapult;
  * Numbers need to be in E.164 format
  */
 final class PhoneNumber extends Types {
-	public function __construct($number)
-	{
-		$m = array();
-		preg_match("/^([0-9\(\)\/\+ \-]*)$/", $number, $m);
+    public function __construct($number)
+    {
+      $m = array();
+      preg_match("/^([0-9\(\)\/\+ \-]*)$/", $number, $m);
 
-		if (!(sizeof($m) > 0))
-			throw new \CatapultApiException("Invalid phone number inputed: " . $number);
-		
-		$this->number = $number;
-	}
+      if (!(sizeof($m) > 0))
+        throw new \CatapultApiException("Invalid phone number inputed: " . $number);
 
-	public function Make($args)
-	{
-		return;
-	}
+      $this->number = $number;
+    }
 
-	public function __toString()
-	{
-		return (string) $this->number;
-	}
+    public function __toString()
+    {
+      return (string) $this->number;
+    }
 }
 
 ?>
