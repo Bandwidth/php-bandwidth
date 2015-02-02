56a45ae9340885a38c5b2d40722779e33902dbf7
Nadir Hamid <matrix.nad@gmail.com>
2015-02-02 03:58:16 +0000

    fix padding

--- source/events/Base.php
+++ source/events/Base.php
@@ -15,27 +15,29 @@ namespace Catapult;
  * by it.  
  */
 class Event {
-	public function __construct($data=null) 
-	{
-		if (!(in_array($_SERVER['HTTP_METHOD'], array("GET", "POST"))))
- 		    throw new \CatapultApiException("Catapult events only accept GET and POST");
+    public function __construct($data=null) 
+    {
+      if (!(in_array($_SERVER['HTTP_METHOD'], array("GET", "POST"))))
+        throw new \CatapultApiException("Catapult events only accept GET and POST");
 
 
-		if ($data!=null) // mocking tests only
-			$data = Ensure::Input($data);
-		else
-			$data = Ensure::Input(json_decode(file_get_contents("php://input")));
+      if ($data!=null) {// mocking tests only
+       $data = Ensure::Input($data);
+      } else {
+        $data = Ensure::Input(json_decode(file_get_contents("php://input")));
+      }
 
-		return new EventType(
-			new EventResource($this, $data)
-		);
-	}
-	public function __call($function, $args) {
-		return $this->model->$function($args);
-	}
-	public function __get($key) {
-		return $this->model->$key;
-	}
+      return new EventType(
+      new EventResource($this, $data)
+      );
+
+    }
+    public function __call($function, $args) {
+      return $this->model->$function($args);
+    }
+    public function __get($key) {
+      return $this->model->$key;
+    }
 }
 
 /**
@@ -53,76 +55,84 @@ class Event {
  * 
  */
 class EventType extends Event {
-	public function __construct($resource=NULL)
-	{ 
-		/**
-	   * when we're called directly as 
-     * a sub class we figure out our input first.
-     
-		 * i.e
-     * event = new Event;
-     *
-     * this is only when the user doesn't knows which
-     * event should be triggered in his program.
-     * or when multiple events lead to the same script
-     *
-     */
-	}
+    public function __construct($resource=NULL)
+    { 
+      /**
+      * when we're called directly as 
+      * a sub class we figure out our input first.
 
-	/**
-	 * TODO:
-	 * models provide stringify functionality
-	 * which resembles:
-   *
-	 * CLASS(op1, op2, op3)
-   *
-   * --- event information should be included 
-	 */
-	public function __toString()
-	{
-		return (string) $this->model;
-	}
+      * i.e
+      * event = new Event;
+      *
+      * this is only when the user doesn't knows which
+      * event should be triggered in his program.
+      * or when multiple events lead to the same script
+      *
+      */
+    }
+
+    /**
+    * TODO:
+    * models provide stringify functionality
+    * which resembles:
+    *
+    * CLASS(op1, op2, op3)
+    *
+    * --- event information should be included 
+    */
+    public function __toString()
+    {
+      return (string) $this->model;
+    }
 
 
-	/**
-	 * _init will register things
-	 * used by this event object
-	 *
-	 */
-	public function _init($args, $class) 
-	{
+   /**
+    * _init will register things
+    * used by this event object
+    *
+    * @param args: EnsuredInput
+    * @param class: Intiated model class
+    */
+    public function _init($args, $class) 
+    {
 
-			$this->model = $class;
-		
-		  $args = $args->get();	
-			$args = Cleaner::Omit($args);
+      $this->model = $class;
+      $args = $args->get();	
+      $args = Cleaner::Omit($args);
 
-			$this->model->id = $args['id'];
-			$this->model->get();
-	}
+      $this->model->id = $args['id'];
+      $this->model->get();
+    }
 
-	/**
-	 * Event calls should be forwaded
- 	 * to their models
-	 *
-	 *
-	 */	
-	public function __call($function, $args)
-	{
-		/**
-		 * Handling will be done
-		 * by GenericResource
-		 */
-		return $this->model->$function($args);
-	}
-	/**
-	 * Same for properties all should
-	 * be identical
-   */
-	public function __get($prop)
-	{
-		return $this->model->$prop;
-	}
+   /**
+    * Event calls should be forwaded
+    * to their models
+    *
+    * This will be identical to 
+    * Call->create
+    * where create is any function of the public API
+    *
+    * @param function: model function
+    * @param args: args for function
+    */	
+    public function __call($function, $args)
+    {
+      /**
+      * Handling will be done
+      * by GenericResource
+      */
+      return $this->model->$function($args);
+    }
+    /**
+    * Same for properties all should
+    * be identical
+    *
+    * @param prop: property for object
+    */
+    public function __get($prop)
+    {
+      return $this->model->$prop;
+    }
 }
 
 ?>
--- source/events/ConferenceMemberEvent.php
+++ source/events/ConferenceMemberEvent.php
@@ -16,9 +16,9 @@
 namespace Catapult;
 
 final class ConferenceMemberEvent extends ConferenceEventMixin {
-	public function __construct() {
-    $data = Ensure::Input(json_decode(file_get_contents("php://input"));
-		return new ConferenceMember($data);
-	}
+    public function __construct() {
+      $data = Ensure::Input(json_decode(file_get_contents("php://input"));
+      return new ConferenceMember($data);
+    }
 }
 ?>
