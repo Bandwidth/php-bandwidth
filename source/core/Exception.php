b7ebeb5cbecacec1b5513479e5e6332444dd4359
Nadir Hamid <matrix.nad@gmail.com>
2015-02-02 04:32:22 +0000

    fix padding

--- source/core/Exception.php
+++ source/core/Exception.php
@@ -25,11 +25,11 @@ final class CatapultApiException extends \Exception
     parent::__construct($msg, $code);
 
     if (Catapult\Log::isOn()) {
-	    $trace = $this->getTrace();
-	    /** get the last line we got an error on, would be the user's file **/
+      $trace = $this->getTrace();
+      /** get the last line we got an error on, would be the user's file **/
 
-	    $line = $trace[sizeof($trace) - 1]['line'];
-    	Catapult\Log::write(time(), "line: " . $line, $this->result);
+      $line = $trace[sizeof($trace) - 1]['line'];
+      Catapult\Log::write(time(), "line: " . $line, $this->result);
     }
   }
 
