aa3ff70108b31944d890f64b50186978d804b283
Nadir Hamid <matrix.nad@gmail.com>
2015-03-21 00:27:40 +0000

    update CHANGELOG

--- examples/sample-client.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once("../source/Catapult.php");
-// below is a sample of Catapult's client
-//
-// for starting off you can consult this example
-// This example uses 0.7.0
-// please note Credentials object is not here
-// and does not need to be used anymore
-
