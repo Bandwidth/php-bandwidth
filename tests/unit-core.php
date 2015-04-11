2032de71c3790168d23f9d347b2ac737b97ca201
Nadir Hamid <matrix.nad@gmail.com>
2015-04-11 00:12:00 -0700

    minor update TBA until MockClass written

--- tests/unit-core.php
+++ tests/unit-core.php
@@ -8,6 +8,10 @@
  * URIResource
  * ResolverResource
  * utils
+ *  
+ * TODO needs more testing:
+ * should test:
+ * loadsResource, schema and meta
  */
 
 $cred = new Catapult\Credentials;
@@ -15,43 +19,59 @@ $client = new Catapult\Client($cred);
 
 
 class CoreTest extends PHPUnit_Framework_TestCase {
-	public function testURIResource()
-	{
-		$url = new Catapult\URIResource("http://google.com/");
-
-		$this->assertEquals((string)$url, "http://google.com/");
-	}
-
-	public function testEnsureResource()
-	{
-		$reference = array(); // must be passed by reference
-		$data = Catapult\Ensure::Input($reference);
-
-		$this->assertTrue($data instanceof Catapult\DataPacket);
-	}
+  public function testURIResource()
+  {
+    $url = new Catapult\URIResource("http://google.com/");
+    $this->assertEquals((string)$url, "http://google.com/");
+  }
+	
+  public function testEnsureResource()
+  {
+     $reference = array(); // must be passed by reference
+     $data = Catapult\Ensure::Input($reference);
+	
+     $this->assertTrue($data instanceof Catapult\DataPacket);
+  }
 
   // constructor 'should' copy an objects state
   // with reference as well as return a new one
   public function testConstructorResource()
   {
     $model = new Catapult\MockModel;
+    $model = new \stdClass;
+    $model->schema = array(
+        "fields" => array(
+          "id"	
+        ),
+        "needs" => array(
+          "id"	
+        )
+    );
     $model->id = "__TEST__";
     $model1 = Catapult\Constructor::make($model);
 
-    $this->assertEquals($model->id);
+    $this->assertEquals($model->id, $model1->id);
   }
 
   // resolver needs to call create or get
   // depending on what we give as parameters
   public function testResolverResource()
   {
-    $model = new Catapult\MockModel;
-
-    // this should be a get
-    Resolver:Find($model, "");
-
-    // this should be a post
-    Resolver::Find($model, array( "param1" => "value1" )); 
+    $model = new \stdClass;
+    $model->schema = array(
+       "fields" => array(
+       	  "id"
+       	),
+       	"needs" => array(
+       	   "id"	
+        )
+    );
+    $model->loads = new \stdClass;
+    $model->loads->init = "";
+    $model->loads->primary = "GET";
+    $model->loads->id = "id";
+    
+   // TODO test resolver resource here
   }
 }
 
